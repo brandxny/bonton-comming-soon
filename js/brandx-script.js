b6d93951f2b80b1eed175e0f4da65712cfb9a85b
juanmriosv <juanma@72-ec-jr3.myfiosgateway.com>
2022-04-08 17:07:34 -0400

    key update

--- js/brandx-script.js
+++ js/brandx-script.js
@@ -3,7 +3,8 @@ var obj = {
     '9G7-wafyUsNQ4y4pu41iiJ-NwDoZDAmBNel1YYxChnY' : 'key2',
     'BywnXD-Scg78p0TznWvf_BqaEzajemRtrCkAksy92FA' : 'key3',
     '9ad80b14098bcead9c7de952435e937cc3723ae61084ba8e729adb642daf0251' : 'key4',
-    '8e31e45f4a0e8959d456ba2914723451b8262337f75bcea2e04ae535491df16d' : 'key5'
+    '8e31e45f4a0e8959d456ba2914723451b8262337f75bcea2e04ae535491df16d' : 'key5',
+    'bf1472d64be4609a8f20ff8ffb1063e074d71094323c979d15c011197df58048' : 'key6'
 }
   
 const keys = Object.keys(obj)
